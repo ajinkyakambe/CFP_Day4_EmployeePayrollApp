e3f382424fcdb73c94e99e601161e0a9e43c258a
ajinkya kambe <ajinkyakambe@gmail.com>
2022-06-17 12:57:47 +0530

    Spring Employee Payroll App Development

---
+++ src/main/java/com/bridgelabz/cfp_day4_employeepayrollapp/CfpDay4EmployeePayrollAppApplication.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.cfp_day4_employeepayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+@SpringBootApplication
+public class CfpDay4EmployeePayrollAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfpDay4EmployeePayrollAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bridgelabz/cfp_day4_employeepayrollapp/CfpDay4EmployeePayrollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.cfp_day4_employeepayrollapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfpDay4EmployeePayrollAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
